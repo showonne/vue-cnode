678c1927c6c3b41101b3786f0e0f0632563db4b7
陈景辉 <918677896@qq.com>
2016-06-26 22:54:02 +0800

    add transition

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,15 +1,54 @@
 <template>
     <div class="bg">
-        <img src="../assets/cnodejs_light.svg" />
+        <img src="../assets/cnodejs_light.svg" v-show="show" transition="fade-ttb" />
         <span>Pwoered by:</span>
         <div class="framework">
-            <img src="../assets/logo.png" />
-            <img src="../assets/mint-ui.svg" />
+            <img src="../assets/logo.png" transition="fade-ltr" v-show="show" />
+            <img src="../assets/mint-ui.svg" transition="fade-rtl" v-show="show" />
         </div>
         <button type="button" v-link="'list'">Enter</button>
     </div>
 </template>
+<script>
+    export default{
+        data() {
+            return {
+                show: false
+            }
+        },
+        ready() {
+            let that = this
+            setTimeout(() => {
+                that.show = true
+            }, 1000)
+        }
+    }
+</script>
 <style lang='less'>
+    .fade-ttb{
+        &-transition{
+            transition: all .5s ease;
+        }
+        &-enter{
+            transform: translateY(-100%);
+        }
+    }
+    .fade-ltr{
+        &-transition{
+            transition: all .5s .5s ease;
+        }
+        &-enter{
+            transform: translateX(-200%);
+        }
+    }
+    .fade-rtl{
+        &-transition{
+            transition: all .5s .5s ease;
+        }
+        &-enter{
+            transform: translateX(200%);
+        }
+    }
     .bg{
         background: #e1eae6;
         height: 100%;
@@ -21,11 +60,11 @@
             width: 90%;
         }
         button{
-            color: #fff;
+            color: #2C3E50;
             background: inherit;
             padding: .5em;
             width: 50%;
-            border: 2px solid #fff;
+            border: 2px solid #2C3E50;
             font-size: 18px;
             outline: none;
             margin-top: 40%;
