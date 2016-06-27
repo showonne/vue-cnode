402ab3239cc20ad5cca34d3aff407274236a330e
陈景辉 <918677896@qq.com>
2016-06-27 10:30:03 +0800

    ad some md style

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -5,13 +5,17 @@
     </mt-header>
     <div class="body">
         <h1 class="title">{{content.title}}</h1>
-        <div class="">
-            <img :src="content.author.avatar_url" />
-            <span>{{content.author.loginname}}</span>
-            <span>{{content.create_at | date}}</span>
-            <span>{{content.visit_count}}</span>
+        <div class="summary">
+            <div class="author">
+                <img :src="content.author.avatar_url" />
+                <span>{{content.author.loginname}}</span>
+            </div>
+            <div class="changes">
+                <span>{{content.create_at | date}}</span>
+                <span>{{content.visit_count}}人浏览过</span>
+            </div>
         </div>
-        <div>{{{content.content}}}</div>
+        <div class="markdown-container">{{{content.content}}}</div>
     </div>
 </template>
 
@@ -64,11 +68,84 @@
     }
 </script>
 
-<style lang='less' scoped>
+<style lang='less'>
     .body{
         padding: 10px 10px;
     }
     .title{
         color: #2c3e50;
+    }
+    .summary{
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        padding: 10px 0;
+        .author{
+            font-size: 16px;
+            img{
+                height: 40px;
+                width: 40px;
+            }
+        }
+        .changes{
+            font-size: 12px;
+        }
+    }
+    .markdown-container{
+        word-wrap: break-word;
+        pre{
+            overflow: auto;
+            &.prettyprint{
+                background: #eee;
+                padding: 10px;
+            }
+        }
+        h1{
+            border-bottom: 1px solid #ddd;
+            padding-bottom: 4px;
+            margin: 1em 0;
+        }
+        h2{
+            margin: 1em 0;
+            border-bottom: 1px solid #ddd;
+            padding-bottom: 4px;
+        }
+        p{
+            line-height: 1.2;
+            margin: 1em 0;
+        }
+        ul{
+            list-style: circle;
+            padding-left: 20px;
+            margin: 4px 0;
+            li{
+                margin: 2px 0;
+            }
+        }
+        ol{
+            list-style: circle;
+            padding-left: 20px;
+            margin: 4px 0;
+            li{
+                margin: 2px 0;
+            }
+        }
+        table{
+            margin: 10px 0;
+            border-collapse: collapse;
+            td, th{
+                padding: 2px 4px;
+                border: 1px solid #ccc;
+            }
+        }
+        blockquote{
+            border-left: 4px solid #ccc;
+            padding: 4px 0 4px 15px;
+            background: #eee;
+            p{
+                margin: 0;
+                color: #333;
+            }
+        }
     }
 </style>
